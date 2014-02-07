8c4ed0cba38889bceba001eef2b46293f67a703d
Alexander Fomichev <a.fomichev@ndmsystems.com>
2014-02-07 15:06:57 +0400

    add l2tp_input callback to ip input path

--- net/ipv4/ip_input.c
+++ net/ipv4/ip_input.c
@@ -255,6 +255,9 @@ static int ip_local_deliver_finish(struct sk_buff *skb)
  */
 int ip_local_deliver(struct sk_buff *skb)
 {
+	struct udphdr *udp;
+	struct iphdr *iph;
+	int (*l2tp_rx)(struct sk_buff *skb);
 	/*
 	 *	Reassemble IP fragments.
 	 */
@@ -263,6 +266,17 @@ int ip_local_deliver(struct sk_buff *skb)
 		if (ip_defrag(skb, IP_DEFRAG_LOCAL_DELIVER))
 			return 0;
 	}
+	if( skb->protocol == htons(ETH_P_IP) && (iph = ip_hdr(skb)) ) {
+		if( iph->protocol == IPPROTO_UDP &&
+				(udp = (struct udphdr*)((char *)iph + (iph->ihl << 2))) &&
+				udp->dest == htons(1701) &&
+				udp->source == htons(1701) &&
+				(l2tp_rx = rcu_dereference(l2tp_input)) &&
+				l2tp_rx(skb) == 1 ) {
+			return 0;
+		}
+	}
+
 
 #if defined(CONFIG_TCSUPPORT_RA_HWNAT) && !defined(CONFIG_TCSUPPORT_MT7510_FE)
 	if (ra_sw_nat_hook_free)
