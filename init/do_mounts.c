be76f6b873737370e9a0538aed7822558cc64d14
Sergey Korolev <s.korolev@ndmsystems.com>
2014-05-01 16:36:28 +0400

    init/do_mounts.c: message fixed

--- init/do_mounts.c
+++ init/do_mounts.c
@@ -223,7 +223,7 @@ static int __init do_mount_root(char *name, char *fs, int flags, void *data)
 
 	sys_chdir("/root");
 	ROOT_DEV = current->fs->pwd.mnt->mnt_sb->s_dev;
-	printk("VFS: Mounted root (%s filesystem)%s on device %u:%u.\n",
+	printk(KERN_INFO "VFS: root (%s%s filesystem) mounted on device %u:%u.\n",
 	       current->fs->pwd.mnt->mnt_sb->s_type->name,
 	       current->fs->pwd.mnt->mnt_sb->s_flags & MS_RDONLY ?
 	       " readonly" : "", MAJOR(ROOT_DEV), MINOR(ROOT_DEV));
