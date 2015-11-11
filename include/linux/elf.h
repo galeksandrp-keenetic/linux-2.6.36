d3392069b87e0128de1ef7069bb674cd0ccaea1a
Dmitry Pichugin <d.pichugin@ndmsystems.com>
2015-11-11 08:00:26 +0300

    include/elf.h: add define elf_sym for 32 and 64 structs (case #6809)

--- include/linux/elf.h
+++ include/linux/elf.h
@@ -421,6 +421,7 @@ extern Elf32_Dyn _DYNAMIC [];
 #define elf_note	elf32_note
 #define elf_addr_t	Elf32_Off
 #define Elf_Half	Elf32_Half
+#define elf_sym		elf32_sym
 
 #else
 
@@ -431,6 +432,7 @@ extern Elf64_Dyn _DYNAMIC [];
 #define elf_note	elf64_note
 #define elf_addr_t	Elf64_Off
 #define Elf_Half	Elf64_Half
+#define elf_sym		elf64_sym
 
 #endif
 
